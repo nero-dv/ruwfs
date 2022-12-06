e164a6b2bc3e52cf03c99d704715cee3d2e0a0a6
nero-dv <louis_noir@protonmail.com>
2022-12-06 11:06:52 -0600

    Forgot a few things

--- install.sh
+++ install.sh
@@ -17,56 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
-# The [ -t 1 ] check only works when the function is not called from
-# a subshell (like in `$(...)` or `(...)`, so this hack redefines the
-# function at the top level to always return false when stdout is not
-# a tty.
-if [ -t 1 ]; then
-  is_tty() {
-    true
-  }
-else
-  is_tty() {
-    false
-  }
-fi
-
 setup_color() {
-  # Only use colors if connected to a terminal
-  if ! is_tty; then
-    FMT_RAINBOW=""
-    FMT_RED=""
-    FMT_GREEN=""
-    FMT_YELLOW=""
-    FMT_BLUE=""
-    FMT_BOLD=""
-    FMT_RESET=""
-    return
-  fi
-
-  if supports_truecolor; then
-    FMT_RAINBOW="
-      $(printf '\033[38;2;255;0;0m')
-      $(printf '\033[38;2;255;97;0m')
-      $(printf '\033[38;2;247;255;0m')
-      $(printf '\033[38;2;0;255;30m')
-      $(printf '\033[38;2;77;0;255m')
-      $(printf '\033[38;2;168;0;255m')
-      $(printf '\033[38;2;245;0;172m')
-    "
-  else
-    FMT_RAINBOW="
-      $(printf '\033[38;5;196m')
-      $(printf '\033[38;5;202m')
-      $(printf '\033[38;5;226m')
-      $(printf '\033[38;5;082m')
-      $(printf '\033[38;5;021m')
-      $(printf '\033[38;5;093m')
-      $(printf '\033[38;5;163m')
-    "
-  fi
-
   FMT_RED=$(printf '\033[31m')
   FMT_GREEN=$(printf '\033[32m')
   FMT_YELLOW=$(printf '\033[33m')
@@ -74,7 +25,6 @@ setup_color() {
   FMT_BOLD=$(printf '\033[1m')
   FMT_RESET=$(printf '\033[0m')
 }
-
 setup_color
 
 command_exists() {
@@ -85,9 +35,8 @@ fmt_error() {
   printf '%sError: %s%s\n' "${FMT_BOLD}${FMT_RED}" "$*" "$FMT_RESET" >&2
 }
 
-if [ ! -V $HOME ] || [ ! -V xdg-user-dir ]; then
-    HOME = /home/$USER
-fi
+HOME = /home/$USER
+RUWFS_DIR = $HOME/.ruwfs
 
 if [ ! -d "$RUWFS_DIR" ]; then
     echo "Creating directory $RUWFS_DIR"
