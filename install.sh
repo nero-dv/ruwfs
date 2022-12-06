10d6b5c0a4cc701fb97249c61a9e46471382b87d
nero-dv <louis_noir@protonmail.com>
2022-12-06 11:11:09 -0600

    fixed a few errors

--- install.sh
+++ install.sh
@@ -17,39 +17,39 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-setup_color() {
-  FMT_RED=$(printf '\033[31m')
-  FMT_GREEN=$(printf '\033[32m')
-  FMT_YELLOW=$(printf '\033[33m')
-  FMT_BLUE=$(printf '\033[34m')
-  FMT_BOLD=$(printf '\033[1m')
-  FMT_RESET=$(printf '\033[0m')
-}
-setup_color
+# setup_color() {
+#   FMT_RED=$(printf '\033[31m')
+#   FMT_GREEN=$(printf '\033[32m')
+#   FMT_YELLOW=$(printf '\033[33m')
+#   FMT_BLUE=$(printf '\033[34m')
+#   FMT_BOLD=$(printf '\033[1m')
+#   FMT_RESET=$(printf '\033[0m')
+# }
+# setup_color
 
 command_exists() {
   command -v "$@" >/dev/null 2>&1
 }
 
-fmt_error() {
-  printf '%sError: %s%s\n' "${FMT_BOLD}${FMT_RED}" "$*" "$FMT_RESET" >&2
-}
+# fmt_error() {
+#   printf '%sError: %s%s\n' "${FMT_BOLD}${FMT_RED}" "$*" "$FMT_RESET" >&2
+# }
 
-HOME = /home/$USER
-RUWFS_DIR = $HOME/.ruwfs
+HOME="/home/$USER"
+RUWFS_DIR="$HOME/.ruwfs"
 
 if [ ! -d "$RUWFS_DIR" ]; then
     echo "Creating directory $RUWFS_DIR"
-    mkdir -p $RUWFS_DIR
-    mkdir -p $RUWFS_DIR/images/archive
-    mkdir -p $RUWFS_DIR/tmp
-    touch $RUWFS_DIR/tmp/install.log
+    mkdir -p ${RUWFS_DIR}
+    mkdir -p ${RUWFS_DIR}/images/archive
+    mkdir -p ${RUWFS_DIR}/tmp
+    touch ${RUWFS_DIR}/tmp/install.log
 fi
 
-command_exists sway || {fmt_error "Sway is not installed. Please install Sway before running this script."; exit 1;}
-command_exists git || { fmt_error "git is not installed"; exit 1;}
-command_exists pip3 || { fmt_error "pip3 is not installed"; exit 1;}
-command_exists python3 || { fmt_error "python3 is not installed"; exit 1;}
+command_exists sway || { echo "Sway is not installed. Please install Sway before running this script."; exit 1;}
+command_exists git || { echo "git is not installed"; exit 1;}
+command_exists pip3 || { echo "pip3 is not installed"; exit 1;}
+command_exists python3 || { echo "python3 is not installed"; exit 1;}
 
 
 git clone --max-depth=1 https://github.com/nero-dv/ruwfs $RUWFS_DIR && cd $RUWFS_DIR
