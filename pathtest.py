300f502582a441ddd43cfa9964333e1198e4ef17
nero-dv <louis_noir@protonmail.com>
2022-12-07 22:23:26 -0600

    Improved performance by not re-running some checks

--- pathtest.py
+++ pathtest.py
@@ -1,21 +1,47 @@
 from pathlib import Path
 
-p = Path.home() / ".ruwfs"
-image_dir = p / "images"
-archive_dir = image_dir / "archive"
-tmp_dir = p / "tmp"
-obuffer = tmp_dir / "outputs.json"
 
-paths = [p, image_dir, archive_dir, tmp_dir]
+def paths():
+    paths
+    home = Path.home() / ".ruwfs"
+    image_dir = home / "images"
+    archive_dir = image_dir / "archive"
+    tmp_dir = home / "tmp"
+    paths = paths.append([home, image_dir, archive_dir, tmp_dir])
+
+    # for path in paths:
+    #     if not path.exists():
+    #         path.mkdir(parents=True)
+
+    obuffer = paths[0][3] / "outputs.json"
+    cached_outputs = paths[0][3] / "cached_outputs.json"
+
+    paths = paths.append([obuffer, cached_outputs])
+
+    for i in paths():
+        print(i)
+
+
+def main():
+    paths()
+    # paths = paths()[0]
+    # files = paths()[1]
+
+    # print(paths)
+    # print(files)
+
 
 # for path in paths:
 #     if not path.exists():
 #         path.mkdir(parents=True) # make the directory and all parent directories
 
-for file in image_dir.glob("*.jpg"):
-    file.rename(archive_dir / file.name)
+# for file in image_dir.glob("*.jpg"):
+#     file.rename(archive_dir / file.name)
 
-if obuffer.is_file():
-    print("yes")
-else:
-    print("no")
+# if obuffer.is_file():
+#     print("yes")
+# else:
+#     print("no")
+
+if __name__ == "__main__":
+    main()
